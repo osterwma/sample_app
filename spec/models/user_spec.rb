551648ea1a1af1aba5fb66dbca5e775093b388d5
osterwma <marvin.osterwold@t-online.de>
2013-12-18 15:33:09 +0100

    End of Chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
-
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -25,22 +25,40 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+
+
 
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
+
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
+
+
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -76,14 +94,18 @@ describe User do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
+
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
+
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -96,6 +118,7 @@ describe User do
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
+
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
@@ -103,6 +126,9 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
-
-
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,8 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title(page_title)) }
   end
 
+
+
   describe "Home page" do
     before { visit root_path }
     let(:heading)    { 'Sample App' }
@@ -18,25 +20,35 @@ describe "Static pages" do
     it { should_not have_selector 'title', text: '| Home' }
   end
 
+
+
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    let(:heading)    { 'Help'}
-    let(:page_title) {'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    let(:heading)    {'About' }
-    let(:page_title) {'About Us'}
+
+    it_should_behave_like "all static pages"
   end
+
+
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    let(:heading)    {'Contact'}
-    let(:page_title) {'Contact'}
+
+    it_should_behave_like "all static pages"
   end
 
   it "should have the right links on the layout" do
@@ -53,4 +65,13 @@ describe "Static pages" do
     click_link "sample app"
     page.should have_selector 'title', text: full_title('')
   end
+
+
 end
+
+
+
+
+
+
+
